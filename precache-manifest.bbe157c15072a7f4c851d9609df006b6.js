85afd68d4f463d7c642a967236d22bfb5373c6f3
Ahmet Uraloglu <ahmeturaloglu@msn.com>
2019-11-21 14:11:33 +0300

    Updates

---
+++ precache-manifest.bbe157c15072a7f4c851d9609df006b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bad673bb34aa98a59a2875710e15571",
+    "url": "/tacas.com.tr/index.html"
+  },
+  {
+    "revision": "3ffc76700fe0d21193b1",
+    "url": "/tacas.com.tr/static/css/main.170f87d9.chunk.css"
+  },
+  {
+    "revision": "079e414094e5fecfc083",
+    "url": "/tacas.com.tr/static/js/2.7566b9bd.chunk.js"
+  },
+  {
+    "revision": "3ffc76700fe0d21193b1",
+    "url": "/tacas.com.tr/static/js/main.285095e9.chunk.js"
+  },
+  {
+    "revision": "f7e1092d852c37fbac5f",
+    "url": "/tacas.com.tr/static/js/runtime-main.d8fca28c.js"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/tacas.com.tr/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/tacas.com.tr/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/tacas.com.tr/static/media/Raleway-SemiBold.17ba6410.ttf"
+  },
+  {
+    "revision": "28140746cc08d802271403ee95f0b40e",
+    "url": "/tacas.com.tr/static/media/assets.28140746.png"
+  },
+  {
+    "revision": "2e956b336d9e429f86515d168a99a397",
+    "url": "/tacas.com.tr/static/media/benefit.2e956b33.png"
+  },
+  {
+    "revision": "a8f941b3f061b3315ad39c0ccdf78af2",
+    "url": "/tacas.com.tr/static/media/bg4.a8f941b3.jpg"
+  },
+  {
+    "revision": "0ea946334d4cccb6f8f1b6ff479dbec0",
+    "url": "/tacas.com.tr/static/media/expert.0ea94633.png"
+  },
+  {
+    "revision": "aae09f8ee15864f4b05c1ce0d95f6eec",
+    "url": "/tacas.com.tr/static/media/goldenhorn_banner.aae09f8e.png"
+  },
+  {
+    "revision": "2d01a941aab8c0a724273f2dd02f58d9",
+    "url": "/tacas.com.tr/static/media/tac_egitim.2d01a941.jpg"
+  }
+]);
