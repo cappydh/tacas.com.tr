17e56b9bd3a324671eebd0309ecf63a6d2f4d933
Ahmet Uraloglu <ahmeturaloglu@msn.com>
2019-11-22 15:51:03 +0300

    modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,28 @@
 /* eslint-disable no-useless-concat */
 import React, { useState } from "react";
+import Modal from "react-modal";
 import VisibilitySensor from "react-visibility-sensor";
+import { FiExternalLink } from "react-icons/fi";
+
+import img1 from "../images/gldnhrn_02.png";
+import img2 from "../images/fit_dg2.JPG";
+import img3 from "../images/tac_egitim.jpg";
 
 import "./Products.css";
 
 const Products = () => {
   const [animateHeader, setAnimateHeader] = useState("");
   const [animateGalery, setAnimateGalery] = useState("");
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsOpen(false);
+  }
 
   const onChangeHeader = isVisible => {
     if (isVisible) {
@@ -33,7 +49,34 @@ const Products = () => {
               <div className="bold">GLDNHRN</div>
               <span className="product-highlight">Business Solution</span>
             </div>
-            <div className="product-button">LEARN MORE</div>
+            <div className="product-button" onClick={handleOpenModal}>
+              LEARN MORE
+            </div>
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={handleCloseModal}
+              className="product-modal"
+            >
+              <img src={img1} alt="img1" className="modal-img" />
+              <p className="modal-project-name">GLDNHRN</p>
+              <p className="modal-project-category">Business Solution</p>
+              <p className="modal-project-description">
+                GLDNHRN is a powerful and comprehensive solution for business
+                process and dynamic case management, which helps organizations
+                to make better-informed decisions about business processes and
+                dynamic cases
+              </p>
+              <div className="modal-button">
+                <FiExternalLink className="linkicon" />
+                <a
+                  className="modal-link"
+                  target="_blank"
+                  href="https://www.gldnhrn.com/index.php"
+                >
+                  VIEW SITE
+                </a>
+              </div>
+            </Modal>
           </div>
         </div>
         <div className="product">
